9a3e4a29f95d86df32a3ef09860ef09bf54aaeb7
a.dedyulya <dziadziulia.a@farnell-service.by>
2026-09-17 13:39:21 +0300

    feat: add S3T3L5 simple_fast_api in docker

--- run.sh
+++ run.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-source .venv_mle-sprint3/bin/activate
+# 1. Автоматически находим, где лежит папка app с файлом main.py
+#TARGET_DIR=$(find /home/mle-user/mle_projects/mle-sprint3/simple_fast_api -type f -path "*/app/main.py" -exec dirname {} \;)
+TARGET_DIR=$(find /home/mle-user/mle_projects/mle-sprint3 -type f -path "*/app/main.py" -exec dirname {} \;)
+echo
+where
+echo
+ls
+echo
+# Если нашли, переходим в родительскую папку для папки app
+if [ -n "$TARGET_DIR" ]; then
+    cd "$TARGET_DIR/.."
+else
+    echo "Ошибка: Не удалось найти папку app/main.py внутри проекта!"
+    exit 1
+fi
 
-uvicorn churn_app:app \
-  --app-dir app \
+# 2. Активируем виртуальное окружение
+source /home/mle-user/mle_projects/mle-sprint3/.venv_mle-sprint3/bin/activate
+
+# 3. Запускаем uvicorn (без --app-dir, так как мы уже внутри нужной папки)
+uvicorn app.main:app \
   --host 0.0.0.0 \
   --port 8000 \
   --reload
